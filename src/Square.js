423eb28d745a0ce2907e323a92c9258f833072fd
PatWyzlic <96600690+PatWyzlic@users.noreply.github.com>
2022-02-22 00:04:32 -0500

    Creation of simple tik tak toe game

---
+++ src/Square.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Square = ({value, onclick}) => {
+    const style = value ? 'squares ${value}' : 'squares';
+
+    return (
+        <button className = {style} onClick = {onclick}>
+            {value}
+        </button>
+    )
+
+}
+
+export default Square;
