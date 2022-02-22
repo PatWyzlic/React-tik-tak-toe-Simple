cb3f086efac10ea816f8c4b8f9b8aa21efc0961b
PatWyzlic <96600690+PatWyzlic@users.noreply.github.com>
2022-02-22 00:51:53 -0500

    Proper functioning

--- src/Game.js
+++ src/Game.js
@@ -31,7 +31,7 @@ const Game = () => {
 
     const renderMoves= () =>
         history.map((_step, move) => {
-            const destination = move ? "Go to move ${move}" : "Go to start";
+            const destination = move ? "Go to move: " + move : "Go to start";
             return(
                 <li key ={move}>
                     <button onClick={() => jumpTo(move)}>{destination}</button> 
